6135f2c40cbf1d91f53cc95a671c2e82202eeb53
Nasser Sanou <nassersanou23@gmail.com>
2018-08-24 18:49:26 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -211,7 +211,7 @@ puts shoe_size("DeSagna Diop")
 puts " _____________Number____________"
 def player_numbers(team_name)
    numbers = [] 
-   puts get_players = call_team(team_name)[:players]
+   p get_players = call_team(team_name)[:players]
   # get_numbers = call_team(team_name).find { |player_name| player_name.fetch(:number) == }
      
    
