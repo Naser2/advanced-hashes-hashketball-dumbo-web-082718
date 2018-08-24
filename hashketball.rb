ff9cdb151261fcd42211ed3c8d0df3dcfa0478ee
Nasser Sanou <nassersanou23@gmail.com>
2018-08-24 19:33:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,7 +219,7 @@ def player_numbers(team_name)
 puts " ____________________PLAYER STATS _______________________"
 def player_stats(player_name)
   details = all_players_with_stats.find {|player | player.fetch(:player_name) == player_name}
-  # details.fetch( :number ..:slam_dunks)
+ details.map { |items| items }
 end
 
 puts player_stats("Alan Anderson")
